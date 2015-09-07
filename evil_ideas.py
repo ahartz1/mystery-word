003c026e2a502d80af81f721660df0d9a68b1e11
Adam Hartz <ahartz1@gmail.com>
2015-09-07 10:39:06 -0400

    Headway on assessment approach.

--- evil_ideas.py
+++ evil_ideas.py
@@ -1,6 +1,6 @@
-def evil_word_selection(constraints, guessed_letters, evil_word_list):
-    '''|_ Because we aren't using a single word, constraints is our "word" _|'''
-    repeats = []
+def evil_word_selector(constraints, guessed_letters, evil_word_list):
+    '''Because we aren't using a single word, our "word" is constraints'''
+    repeats = []  # list of lists; index represents the # of times letter appears
     for word in evil_word_list:
       letter_count = list(word).count(guessed_letters[0][-1])
       if letter_count > 0:
@@ -18,8 +18,21 @@ def assess_open_slots(word, constraints, guessed_letters, evil_word_list):
 constraints = display_word(guessed_letters[1])
 
 '''
+Gameplay Ideas:
 It might be nice to output how many words were eliminated from the total for each
 guess. Also, show the remaining words over the total number of words in the
 dictionary; display the percentage of words remaining and percentage eliminated
 by previous letter guessed.
+'''
+
+'''
+evil_word_selector optimization ideas:
+If any list of duplicates is longer than both the non-letter-containing list and
+the single-letter-containing list:
+  - instead of looking at all permutations, use an empirical approach: take the
+    first word and look for the number of other words in the list with that
+    configuration of duplicates. Then, assess the length of that list against the
+    next longest list (first time around, this would be the single-letter-containing
+    list because the non-letter-containing list has to be shorter to start the
+    evaluation process to begin with.
 '''
