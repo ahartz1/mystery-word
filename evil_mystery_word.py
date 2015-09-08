13b279912734769d7e21cb9a6ddae81b93c8512b
Adam Hartz <ahartz1@gmail.com>
2015-09-07 20:21:35 -0400

    Game mostly working, just needs to return a word from evil_word_list if player loses.

--- evil_mystery_word.py
+++ evil_mystery_word.py
@@ -2,40 +2,36 @@ import random
 from os import get_terminal_size
 
 
-def easy_words(word_list):
+def easy_words():
     """
-    Returns a filtered version of the word list with words only containing
-    4-6 characters.
+    Returns empty constraints 4-6 characters long.
     """
-    easy_word_list = []
-    for word in word_list:
-        if len(word) >= 4 and len(word) <=6:
-            easy_word_list.append(word)
-    return easy_word_list
+    length = random.randint(4,7)
+    constraints = init_constraint(length)
+    return ''.join(constraints)
 
 
-def medium_words(word_list):
+def medium_words():
     """
-    Returns a filtered version of the word list with words only containing
-    6-8 characters.
+    Returns empty constraints 6–8 characters long.
     """
-    medium_word_list = []
-    for word in word_list:
-        if len(word) >= 6 and len(word) <=8:
-            medium_word_list.append(word)
-    return medium_word_list
+    length = random.randint(6,9)
+    constraints = init_constraint(length)
+    return ''.join(constraints)
 
 
 def hard_words(word_list):
     """
-    Returns a filtered version of the word list with words only containing
-    8+ characters.
+    Returns empty constraints between 8 and dict_max characters.
     """
-    hard_word_list = []
+    dict_max = 0
     for word in word_list:
-        if len(word) >= 8:
-            hard_word_list.append(word)
-    return hard_word_list
+        if len(word) > dict_max:
+            dict_max = len(word)
+
+    length = random.randint(8, dict_max)
+    constraints = init_constraint(length)
+    return ''.join(constraints)
 
 
 def random_word(word_list):
@@ -45,27 +41,17 @@ def random_word(word_list):
     return word_list[random.randint(0,len(word_list))].lower()
 
 
-def display_word(game_word, guessed_letters):
-    """
-    Returns a string that including blanks (_) and letters from the given word,
-    filling in letters based upon the list of guesses.
+def init_constraint(length):
+    constraints = []
+    for n in range(length):
+        constraints.append('_')
+    return ''.join(constraints)
 
-    There should be spaces between each blank _ and each letter. Each letter
-    should be capitalized for display.
 
-    For example, if the word is BOMBARD and the letters guessed are a, b,
-    and d, this function should return 'B _ _ B A _ D'.
-    """
-    # Have a collector list, insert upper() of iteration variable if present
-    # insert '_' if not in word
+def display_word(constraints):
     display = []
-
-    for char in game_word:
-        if char in guessed_letters:
-            display.append(char.upper())
-        else:
-            display.append('_')
-    return ' '.join(display)
+    display = ' '.join(list(constraints)).upper()
+    return display
 
 
 def show_guessed_letters(guessed_letters):
@@ -86,121 +72,147 @@ def show_guessed_letters(guessed_letters):
         print('Guessed Consonants: {}'.format(' '.join(sorted(consonants)).upper()))
 
 
-def is_word_complete(game_word, guessed_letters):
+def is_word_complete(constraints):
     """
-    Returns True if the list of guesses covers every letter in the word,
-    otherwise returns False.
+    Returns True if the constraints have no blanks left.
     """
-    is_complete = True
-    for char in game_word:
-        if char not in guessed_letters:
-            is_complete = False
+    is_complete = False
+    if '_' not in constraints:
+        is_complete = True
     return is_complete
 
 
-def evil_word_selector(game_word, guessed_letters, evil_list):
-    '''
-    evil_list begins as a list of words that are of game_word length.
-    1. A new random generator is needed for this version; choose word length only.
-         - Need to look at distribution of words at different lengths to see
-           what length should be the upper cutoff for game play
-    2. Because we are going to return the pared down list, we need only assess
-       the incoming letter.
-    3. Look at the incoming list and make two lists for each empty position:
-         a. one that has the guessed letter (yes_guessed_letter)
-            - requires that each word be checked for duplicates that might
-                  interfere with non-blanks.
-            - any word that has duplicates must then be checked against the number
-              of words left after the duplicated position has been filled
-         b. one that does not have the guessed letter (no_guessed_letter)
-    4. Assess which list is largest.
-    5. If the larger of the lists includes the guessed letter, include it wherever
-        it appears, otherwise, exclude it.
-    '''
-    word_template = display_word(game_word, guessed_letters[:-1])
-    evil_options = []
-    evil_options[0] = evil_list[:]
+def evil_word_selector(constraints, guessed_letters, evil_word_list):
+    '''Because we aren't using a single word, our "word" is constraints'''
+    # dict of lists; key represents # of viable letter occurrences
+    repeats = {0: {'words': []}} # may need 'pattern'='__*_'
+    # was {0: {'words': [], 'use': False}}
+    letter_count = 0   # number of viable letter occurences
+    most_words = 0     # placeholder dictionary to hold
+    selected_dict = {} # holds largest dictionary
+    new_constraints = ''
 
-    # Make a list of lists, each containing narrower set of the last
-    for i, char in enumerate(word_template):
-        if char != '_':
-            for word in evil_options[i]:
-                if char == word[i]:
-                    evil_options[i+1].extend(list(word))
+    for word in evil_word_list:
+        letter_count = list(word.lower()).count(guessed_letters[-1])
+        if letter_count > 0:
+            if not repeats.get(letter_count, False):
+                repeats[letter_count] = {}
+            useable_word, new_constraints = assess_open_slots(
+                              word.lower(), guessed_letters[-1], constraints)
+            if useable_word:
+            # Now, put into a list keyed with the new constraint
+            #(e.g., if our letter is 'e', the key would be '_e__')
+                if not repeats[letter_count].get(new_constraints, False):
+                    repeats[letter_count][new_constraints] = []
+                repeats[letter_count][new_constraints].append(word.lower())
+        else:
+            repeats[0]['words'].append(word) # list of words without guessed letter
 
-    # Make a selection from the pared down list
-    while True:
-        evil_word = random_word(evil_options[-1])
-        if evil_word = game_word
+    # print(repeats)
+    # Assess length of lists
+    for num_repeats, num_repeats_dict in repeats.items():
+        for constraints_pattern, words in num_repeats_dict.items():
+            if len(words) > most_words:
+                most_words = len(words)
+                selected_dict = {'new_constraints': constraints_pattern,
+                                 'words': words, 'length': len(words)}
 
-    return evil_word, evil_options[-1]
+    # # Make decision about how to proceed
+    if selected_dict['length'] == len(repeats[0]['words']):
+        evil_word_list = repeats[0]['words']
+        new_constraints = constraints
+    else:
+        evil_word_list = selected_dict['words']
+        new_constraints = selected_dict['new_constraints']
+
+    # print(repeats)
+    # print("Letter: {}\nTotal words: {}".format(guessed_letters[0][-1], len(evil_word_list)))
+    # print("New constraints: {}\nGuessed letters: {}\nRemaining words: {}".format(
+    #     new_constraints, guessed_letters, evil_word_list))
+    return new_constraints, guessed_letters, evil_word_list
 
 
-# def evil_list_reducer(word_template, prev_guessed_letters, evil_list):
-#
-#     return reduced_evil_list
+def assess_open_slots(word, letter, constraints):
+    available_indices = []
+    usable = True
+    new_constraints = list(constraints)
+
+    for constraint_index, constraint_char in enumerate(list(constraints)):
+        if constraint_char == '_':
+            available_indices.append(constraint_index)
+
+    for index, char in enumerate(list(word.lower())):
+        if letter == char:    # look for location of guess letter in word
+            if index not in available_indices: # letter can only appear in '_' slots:
+                new_constraints = list(constraints)
+                return False, constraints
+            else:
+                new_constraints[index] = letter
+
+    return usable, ''.join(new_constraints)
 
 
 def game_mode(word_list):
-    '''Queries user for game mode and returns game_word'''
-    game_word = ''
+    '''Queries user for game mode and returns constraints'''
+    constraints = ''
     while True:
-        print("\nPlease select your level of difficulty (press Q to quit):")
-        difficulty_level = input( "[E]asy, [M]edium, or [H]ard\n> ").lower()
+        print("\nPlease select your word length level of difficulty (press Q to quit):")
+        difficulty_level = input( "[E]asy (4-6), [M]edium (6-8), or [H]ard (8+)\n> ").lower()
         if difficulty_level == 'e':
-            game_word = random_word(easy_words(word_list))
+            constraints = easy_words()
             break
         elif difficulty_level == 'm':
-            game_word = random_word(medium_words(word_list))
+            constraints = medium_words()
             break
         elif difficulty_level == 'h':
-            game_word = random_word(hard_words(word_list))
+            constraints = random_word(hard_words(word_list))
             break
         elif difficulty_level == 'q':
             break
         else:
             print("\nInvalid selection.")
             continue
-    return game_word
+    return constraints
 
 
-def game_loop(game_word, width):
+def game_loop(constraints, width, word_list):
     guessed_letters = []    # holds letters guessed by the user
     allowed_guesses = 8     # number of guesses allotted to user per game (8)
     num_guesses = 0         # number of expended user guesses
     play_again = False      # records whether user wants to play again
     # MAIN GAME LOOP
-    while len(game_word) != 0 and num_guesses < allowed_guesses:
+    while len(constraints) != 0 and num_guesses < allowed_guesses:
         # Print current state of word with underscores for unguessed letters
         print('\n')
-        print(display_word(game_word, guessed_letters).center(width))
-        # print("Test: {}".format(game_word)) # TEST TO REMOVE
+        print(display_word(constraints).center(width))
         print('\n\nYou have {} guesses remaining.'.format(allowed_guesses - num_guesses))
 
         # Get guess from user and add to guessed_letters list
         guessed_letters.append(user_guess(guessed_letters, width))
 
-        # Add to guess tally if letter is not in game_word
-        if guessed_letters[-1] not in game_word:
+        # Run the evil_word_selector
+        constraints, guessed_letters, word_list = evil_word_selector(
+            constraints, guessed_letters, word_list)
+        if guessed_letters[-1] not in constraints:
             num_guesses += 1
 
         # Show guessed letters
         show_guessed_letters(guessed_letters)
 
-        if is_word_complete(game_word, guessed_letters):
-            print(display_word(game_word, guessed_letters).center(width))
+        if is_word_complete(constraints):
+            print(display_word(constraints).center(width))
             win_msg = '    You win!    '.upper()
             win_fill = (width - len(win_msg)*3)//4
             extra_bit = width - len(win_msg)*3 - 4*win_fill
             print(('\n'*4+('='*win_fill+win_msg)*3+'='*(win_fill+extra_bit))+'\n\n')
         elif num_guesses >= allowed_guesses:
-            print(display_word(game_word, guessed_letters).center(width))
+            print(display_word(constraints).center(width))
             no_guesses_msg = 'You have run out of guesses'.upper()
             no_guesses_fill = (width - len(no_guesses_msg))//2
             print('\n'*4+'_'*no_guesses_fill+no_guesses_msg+'_'*no_guesses_fill)
-            print('\nThe word you were trying to guess was: {}\n\n'.format(game_word.upper()))
+            print('\nThe word you were trying to guess was: {}\n\n'.format(random_word(word_list).upper()))
 
-        if is_word_complete(game_word, guessed_letters) or num_guesses >= allowed_guesses:
+        if is_word_complete(constraints) or num_guesses >= allowed_guesses:
             play_again = user_continue(width)
             break
         # Not sure if I need to return both; is there a way to return just one?
@@ -263,7 +275,7 @@ def main():
     5. Giving the user the option to play again
     """
     word_list = []          # holds entire dictionary
-    game_word = ''          # holds random word from specified game mode
+    constraints = ''          # holds random word from specified game mode
     play_again = False      # records whether player wants to play again
     game_start = True       # records whether the game has been played yet
     width = 80              # default width
@@ -291,10 +303,10 @@ def main():
             print(('?' + ' '*(width-2) + '?\n')*((height-7)*2//3))
 
         # Get game word
-        game_word = game_mode(word_list)
+        constraints = game_mode(word_list)
 
         # Run game loop
-        play_again, game_start = game_loop(game_word, width)
+        play_again, game_start = game_loop(constraints, width, word_list)
 
         if play_again:
             continue
